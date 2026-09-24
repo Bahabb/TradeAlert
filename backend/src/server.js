184f3115bbb6bd0a5e97eaebfd307e6ff5720185
Bahabb <timibaha@gmail.com>
2026-09-24 18:26:29 -0500

    SCRUM-29-TEST-Verify-hardcoded-mock-alerts-flow-end-to-end

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,12 +7,28 @@ const { port } = require("./config/env");
 const httpServer = http.createServer(app);
 const io = new Server(httpServer, {  
     cors: {    
-        origin: "http://localhost:5173",  // React app URL
+        origin: "http://localhost:5174",  // React app URL
     },
 });
 
 io.on("connection", (socket) => {  
     console.log("Socket client connected:", socket.id);
+
+    const mockBackendAlerts = [
+        {    
+        id: "3",    
+        timestamp: "2024-06-01T12:00:00Z",    
+        message: "This is a mock alert coming through Socket.IO",  
+        },
+        {    
+        id: "4",    
+        timestamp: "2024-06-01T12:00:00Z",    
+        message: "This is another mock alert coming through Socket.IO",  
+        }
+    ];
+
+
+    socket.emit("backendMockAlert", mockBackendAlerts);
 
     socket.on("disconnect", () => {  
     console.log("Socket client disconnected:", socket.id);
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,37 +1,59 @@
 import AlertFeed from './components/AlertFeed.tsx';
+import { AlertItemType } from './types.ts';
 import { mockData } from './mockData.ts';
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import socket from './socket.tsx';
 
 
 function App() {
+    const [alerts, setAlerts] = useState<AlertItemType[]>([]);
+
 
     useEffect( () => {
-            socket.on('connect', () => {
+
+            function handleConnect() {
                 console.log('Connected to the backend Socket.IO server'); 
-            });
+            }
 
-            socket.on('connect_error', (err) => {
+            function handleConnectError(err: any) {
                 console.error('Socket.IO connection to backend failed, error:', err);
-            });
+            }
 
-            socket.on("disconnect", (reason) => {  
-                console.log('Disconnected from the backend Socket.IO server');
-                console.log('Disconnected from backend Socket.IO server, reason:', reason);
-            });
+            function handleDisconnect(reason: any) {
+                console.log('Disconnected from the backend Socket.IO server, reason:', reason);
+            }
+
+           function handleBackendMockAlert(incomingAlerts: AlertItemType[]) {
+                console.log('Received backendMockAlert event with data:', incomingAlerts);
+
+                setAlerts(
+                    (previousAlerts) => [        
+                        ...previousAlerts,        
+                        ...incomingAlerts,
+                    ]);
+            }
+
+            socket.on('connect', handleConnect);
+
+            socket.on('connect_error', handleConnectError);
+
+            socket.on("backendMockAlert", handleBackendMockAlert);
+
+            socket.on("disconnect", handleDisconnect);
 
             return () => {
                 console.log('Disconnecting from the backend Socket.IO server');
                 socket.off('connect');
                 socket.off('connect_error');
                 socket.off('disconnect');
+                socket.off("backendMockAlert", handleBackendMockAlert);
             }
     }, []);
 
     return(
         <div className="App">
-            <AlertFeed alerts={mockData} />
+            {alerts.length > 0 && <AlertFeed alerts={alerts} />}
         </div>
     );
 
