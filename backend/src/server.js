559f283cc93f65bd08c44f2c9ba77773f122d19f
Bahabb <timibaha@gmail.com>
2026-09-24 12:56:43 -0500

    SCRUM-27-BACKEND-Setup-SocketIO-server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,24 @@
+const http = require("http");
+const { Server } = require("socket.io");
+
 const app = require("./app");
 const { port } = require("./config/env");
 
-app.listen(port, () => {  console.log(`Server running on http://localhost:${port}`);});
+const httpServer = http.createServer(app);
+const io = new Server(httpServer, {  
+    cors: {    
+        origin: "http://localhost:5173",  // React app URL
+    },
+});
+
+io.on("connection", (socket) => {  
+    console.log("Socket client connected:", socket.id); 
+    
+    socket.on("disconnect", () => {  
+    console.log("Socket client disconnected:", socket.id);
+npm });
+});
+
+
+
+httpServer.listen(port, () => {  console.log(`Server running on http://localhost:${port}`);});
